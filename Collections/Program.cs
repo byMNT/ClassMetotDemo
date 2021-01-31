9d340517ab55dc51b240cd1886c277b24d4d5820
Merve Nurfem Taysı <merve.nurfem@gmail.com>
2021-01-31 10:19:37 +0300

    End of Camp 20.01.2021 lesson

---
+++ Collections/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//string [] names = new string [] {"Elif", "Nurfem",	"Bengisu", "Emine"};
+			//Console.WriteLine(names[0]);
+			//Console.WriteLine(names[1]);
+			//Console.WriteLine(names[2]);
+			//Console.WriteLine(names[3]);
+			//names[4] = "Rüveyda";
+			//Console.WriteLine(names[4]);
+
+			List<string> names2 = new List<string> { "Elif", "Nurfem", "Bengisu", "Emine" };
+			Console.WriteLine(names2[0]);
+			Console.WriteLine(names2[1]);
+			Console.WriteLine(names2[2]);
+			Console.WriteLine(names2[3]);
+			names2.Add("Ahmet");
+			Console.WriteLine(names2[4]);
+			Console.WriteLine(names2[0]);
+
+		}
+	}
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace OOP1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Product product1 = new Product();
+			product1.Id = 1;
+			product1.CategoryId = 2;
+			product1.ProductName = "Masa";
+			product1.UnitPrice = 100;
+			product1.UnitsInStock = 3;
+
+			Product product2 = new Product { Id = 2, CategoryId = 3, ProductName = "Kamera", UnitPrice = 10, UnitsInStock = 5};
+		}
+	}
+}
